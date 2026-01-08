022cb62f6f64130e4ea6b3ffab63264b511561c2
Luke Thompson <luke.thompson@noaa.gov>
2026-01-08 16:29:47 -0500

    starting on suffixes to differentiate duplicate sample names

--- faire2qiime.py
+++ faire2qiime.py
@@ -22,10 +22,23 @@ def main(path_faire, absolute_path_sequences, output_directory):
     # Metadata file for each assay_name
     dict_assay_short = {}
     for assay_name in df_exptrun['assay_name'].unique():
+        # For each assay, merge experiment run metadata with sample metadata and project metadata
         metadata = df_exptrun[df_exptrun['assay_name'] == assay_name].merge(df_sample, on='samp_name', how='left', suffixes=('', '_SAMPLE'))
         metadata = pd.merge(metadata, df_project_filled.loc[assay_name].to_frame().transpose(), how='cross', suffixes=('', '_PROJECT'))
         metadata.dropna(axis=1, how='all', inplace=True)
         metadata.rename(columns={'samp_name': 'sample_name'}, inplace=True)
+        
+        # Add suffix to sample names that are duplicated within the assay using column_suffix (if provided), direction_suffix, num_chars_suffix, and delimiter_suffix
+        # duplicated_samples = metadata['sample_name'][metadata['sample_name'].duplicated(keep=False)].unique()
+        # for sample in duplicated_samples:
+        #     sample_rows = metadata[metadata['sample_name'] == sample]
+        #     for idx, row in sample_rows.iterrows():
+        #         suffix = ''
+        #         # Currently not implemented: logic to generate suffix based on provided parameters
+        #         new_sample_name = f"{row['sample_name']}{suffix}"
+        #         metadata.at[idx, 'sample_name'] = new_sample_name
+        
+        # Short assay name to use for output files
         gene = df_project_filled.loc[assay_name]['target_gene'].split(' ')[0]
         subfragment = df_project_filled.loc[assay_name]['target_subfragment']
         subfragment_part = subfragment.split(' ')[0].replace('-', '').replace('_', '') if isinstance(subfragment, str) and subfragment.strip() else ''
@@ -51,5 +64,10 @@ if __name__ == "__main__":
     parser.add_argument('--path_faire', required=True, help='Path to FAIRe Excel file')
     parser.add_argument('--absolute_path_sequences', required=True, help='Absolute path to sequences directory')
     parser.add_argument('--output_directory', required=True, help='Directory to save output files')
+    parser.add_argument('--column_suffix', required=False, help='Column name to get suffix from to differentiate samples (not implemented)', default=None)
+    parser.add_argument('--direction_suffix', required=False, help='Side of text to read from to generate suffixn (not implemented)', choices=['left', 'right'], default='left')
+    parser.add_argument('--num_chars_suffix', required=False, type=int, help='Number of characters to use for suffix (not implemented)', default=20)
+    parser.add_argument('--delimiter_suffix', required=False, help='Delimiter and optionally prefix text to add before suffix text (not implemented)', default='_')
     args = parser.parse_args()
     main(args.path_faire, args.absolute_path_sequences, args.output_directory)
+    
